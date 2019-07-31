ef420fda52d58e18a18d3370ad9714e3575179fe
kevin <1174389158@qq.com>
2019-07-31 13:57:11 +0800

    UDP服务器

---
+++ UdpServer/udpClient.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <stdlib.h>
+
+void Usage(std::string proc)
+{
+  std::cerr << "Usage:" << proc << "  SERVER_IP  SERVER_PORT" << std::endl;
+  std::cerr << "Version:0.0.1" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+  if(argc != 3)
+  {
+    Usage(argv[0]);
+    return 1;
+  }
+  int sock = socket(AF_INET, SOCK_DGRAM, 0);
+  if(sock < 0)
+  {
+    std::cerr << "socket error" << std::endl;
+    return 2;
+  }
+  std::cout << "sock: " << sock << std::endl;
+  struct sockaddr_in server;
+  server.sin_family = AF_INET;
+  server.sin_port = htons(atoi(argv[2]));
+  server.sin_addr.s_addr = inet_addr(argv[1]);
+
+  char buf[1024];
+  for(;;)
+  {
+    std::cout << "Piease Enter# ";
+    std::cin >> buf;
+    sendto(sock, buf, strlen(buf), 0, (struct sockaddr*)&server, sizeof(server)); 
+    struct sockaddr_in peer;
+    socklen_t len = sizeof(peer);
+    ssize_t s = recvfrom(sock, buf, sizeof(buf)-1, 0, (struct sockaddr*)&peer, &len);
+    if(s > 0)
+    {
+      buf[s] = 0;
+      std::cout << "server echo# " << buf << std::endl;
+    }
+  }
+
+  close(sock);
+  return 0;
+}

4b0ad72411db6d3989a2beac9d643ddcdf98530d
kevin <1174389158@qq.com>
2019-08-08 14:14:42 +0800

    多进程版TCP服务器

---
+++ tcp_process/TcpServer.cc
@@ -0,0 +1,20 @@
+#include "TcpServer.hpp"
+
+void Usage(std::string proc)
+{
+  std::cout << "Usage: " << proc << " port" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+  if(argc != 2)
+  {
+    Usage(argv[0]);
+    exit(1);
+  }
+  int port = atoi(argv[1]);
+  TcpServer *tsp = new TcpServer(port);
+  tsp->InitServer();
+  tsp->Start();
+  return 0;
+}

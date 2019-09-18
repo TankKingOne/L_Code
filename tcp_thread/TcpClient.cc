350ea69ee423a235c14ecc7fe2c39cda88fb7036
kevin <1174389158@qq.com>
2019-09-18 17:23:49 +0800

    多线程版TCP服务器

---
+++ tcp_thread/TcpClient.cc
@@ -0,0 +1,21 @@
+#include "TcpClient.hpp"
+
+void Usage(std::string proc)
+{
+  std::cout << "Usage: " << proc << " server_ip server_port" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+  if(argc != 3)
+  {
+    Usage(argv[0]);
+    exit(1);
+  }
+  int port = atoi(argv[2]);
+  std::string ip = argv[1];
+  TcpClient *cli = new TcpClient(ip, port);
+  cli->InitClient();
+  cli->Start();
+  return 0;
+}
